5dad4e9933dd11ac2f0790d6fc8a0aa9b8c49bf9
MarcosRBjunior <marcosribeirojr@alunos.fho.edu.br>
2026-06-30 23:30:53 -0300

    feat: React routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import {createGlobalStyle} from 'styled-components';
 
@@ -30,8 +31,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/categorias" element={<p>Oi!</p>} />
+        <Route path="/favoritos" element={<p>Oi!</p>} />
+        <Route path="/estante" element={<p>Oi!</p>} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
