158575c51e31c7621d93cd745bf2e408094331f0
MarcosRBjunior <marcosribeirojr@alunos.fho.edu.br>
2026-07-02 12:30:18 -0300

    Feat: Book service on the Home page

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,7 @@
 import Input from '../Input';
 import styled from 'styled-components';
-import { useState } from 'react';
-import { livros } from './dadosPesquisa';
+import { useEffect, useState } from 'react';
+import { getLivros } from '../../servicos/livros';
 
 const PesquisaContainer = styled.section`
     display: flex;
@@ -56,6 +56,16 @@ const Resultado = styled.div`
 
 function Pesquisa() {
     const [livrosPesquisados, setlivrosPesquisados] = useState([]);
+    const [livros, setLivros] = useState([]);
+
+    useEffect(() => {
+        fetchLivros();
+    }, []);
+
+    async function fetchLivros() {
+        const livrosDaAPI = await getLivros();
+        setLivros(livrosDaAPI);
+    }
 
     return (
         <PesquisaContainer>
